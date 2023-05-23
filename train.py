142448b549e4843978eeef90cdf9828f9f21bbc7
FarshanaFathima <farshana.fathima@quantrium.ai>
2023-05-23 20:20:02 +0400

    modify forest depth

--- train.py
+++ train.py
@@ -16,8 +16,8 @@ clf.fit(X_train, y_train)
 
 acc = clf.score(X_test, y_test)
 print(acc)
-with open("metrics.txt", "w") as outfile:
-    outfile.write("Accuracy: " + str(acc) + "\n")
+# with open("metrics.txt", "w") as outfile:
+#     outfile.write("Accuracy: " + str(acc) + "\n")
 
 # Plot it
 disp = ConfusionMatrixDisplay.from_estimator(
